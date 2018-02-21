87db7c81f865513d044394e90a3ff8a5642d43a4
jose.encarnacion <jose.encarnacion@LPT-INF-144>
2018-02-21 16:38:02 -0400

    commit all

---
+++ WebApiNCF/WebApiNCF/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebApiNCF.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
